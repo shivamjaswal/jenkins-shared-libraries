69b39113a9b13aeafaa29ed64dcaa3f96af8a7a7
Shivam Jaswal <75036844+shivamjaswal@users.noreply.github.com>
2026-05-24 19:14:58 +0530

    Update Docker build command to use local image tag

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,5 @@
 def call(String ProjectName, String ImageTag, String DockerHubUser){
   sh "mvn clean package"
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+  // sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+  sh "docker build -t ${ProjectName}:${ImageTag} ."
 }
